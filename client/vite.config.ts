bf5da96b3a675982ee50efb78420a3079576b4a5
Fagner Cerqueira <fagner_cerqueira@icloud.com>
2025-02-26 17:29:37 -0300

    Ends project containerization with Docker Compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default ({ mode }: { mode: never }) => {
+   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+   return defineConfig({
+      plugins: [react()],
+      server: {
+         watch: {
+            usePolling: true,
+          },
+         host: true, 
+         strictPort: true,
+         port: 5173,
+         proxy: {
+            '/api': {
+               target: process.env.VITE_APP_API,
+            },
+         },
+      },
+   });
+};
