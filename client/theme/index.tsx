e627168b1c1359d01c52bfc6701167d1ac7de775
Fagner Cerqueira <fagner_cerqueira@icloud.com>
2025-02-27 18:24:32 -0300

    Adds theme provider

---
+++ client/theme/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: blue[500],
+        },
+        secondary: {
+            main: blue[900],
+        },
+    },
+});
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function ThemeProvider({ children }: Props) {
+  return (
+    <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+    </MuiThemeProvider>
+  );
+}
